26ad0f541b574a5d3796be43e3140ecbbb0c90eb
Jason Deakin <jason.deakin.dev@gmail.com>
2017-02-06 05:29:33 +0000

    Visit counter

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import logging
+
 from flask import Flask
+from google.appengine.ext import ndb
 
 app = Flask(__name__)
 
 
+class PageVisit(ndb.Model):
+    timestamp = ndb.DateTimeProperty(auto_now_add=True)
+
+
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    visited_so_far = PageVisit.query().count() + 1
+    visit_record = PageVisit()
+    visit_record.put()
+    return 'Visited {0} times.'.format(visited_so_far)
 
 
 @app.errorhandler(500)
